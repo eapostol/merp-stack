7c62eca9feba47427ad894e378d9e5ebfe8a9b5f
Edward J. Apostol <eapostol@users.noreply.github.com>
2025-04-04 17:35:22 -0400

    feat: install maintine component framework

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HelloFromServer from './HelloFromServer'
+import HelloFromServer from './components/HelloFromServer'
 
 function App() {
   const [count, setCount] = useState(0)
